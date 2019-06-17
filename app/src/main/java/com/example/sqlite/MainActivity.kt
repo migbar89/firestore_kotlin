9554a32d3680d7293db8c7f2b7564772b061d221
Miguel <miguel.lezama89@gmail.com>
2019-06-17 16:48:35 -0600

    Agregando funcion de eliminar

--- app/src/main/java/com/example/sqlite/MainActivity.kt
+++ app/src/main/java/com/example/sqlite/MainActivity.kt
@@ -7,22 +7,16 @@ import android.util.Log
 import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.google.firebase.firestore.EventListener
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.QuerySnapshot
+import com.google.firebase.firestore.*
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
-import com.google.firebase.firestore.FirebaseFirestoreSettings
-
-
 
 
 class MainActivity : AppCompatActivity() {
 
 
-    var listaproducto=ArrayList<producto>()
-    //var db : room? = null
+    lateinit var listaproducto: MutableList<DocumentSnapshot>
 
 
 
@@ -38,69 +32,17 @@ class MainActivity : AppCompatActivity() {
 
         db.collection("productos").
             addSnapshotListener(EventListener<QuerySnapshot>{snapshots, e ->
-               listaproducto.clear()
+
                 if (e != null) {
                     Log.w("Realtime", "listen:error", e)
                     return@EventListener
                 }
-                for (doc in snapshots!!)
-                {
-                    val p = doc.toObject(producto::class.java)
-                    listaproducto.add(p)
-
-                }
-
+                listaproducto= snapshots!!.documents
+                //Toast.makeText(this,listaproducto.size.toString(),Toast.LENGTH_SHORT).show()
                 cargardatos()
 
             });
-   /*  //Cargar datos de firestore
-        db.collection("productos")
-            .get()
-            .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    listaproducto.clear()
-                    for (document in task.result!!) {
-                         val p = document.toObject(producto::class.java)
-                        listaproducto.add(p)
-                    }
-                    cargardatos()
 
-
-                } else {
-                   // Log.w(FragmentActivity.TAG, "Error getting documents.", task.exception)
-                }
-            }
-
-*/
-
-
-
-
-
-
-    btcargar.setOnClickListener {
-
-      /*  db.collection("productos")
-            .get()
-            .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    listaproducto.clear()
-                    for (document in task.result!!) {
-                        val p = document.toObject(producto::class.java)
-                        listaproducto.add(p)
-                    }
-                    cargardatos()
-
-
-                } else {
-                    // Log.w(FragmentActivity.TAG, "Error getting documents.", task.exception)
-                }
-            }*/
-
-       // Toast.makeText(applicationContext,datospublicos.consultaproductos(applicationContext).size.toString(),Toast.LENGTH_SHORT).show()
-
-
-    }
 
 
         btinsertar.setOnClickListener {
@@ -108,28 +50,16 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
        }
 
-
-
-
-
-
-
-
-
     }
 
-    fun insertar()
-    {
-
-    }
 
     fun cargardatos()
     {
         recycleproducto.layoutManager=LinearLayoutManager(applicationContext,LinearLayout.VERTICAL,false)
-        if(listaproducto.size>0) {
+       // if(listaproducto.size>0) {
             var adapter = adapterproducto(listaproducto)
             recycleproducto.adapter = adapter
-        }
+        //}
     }
     override fun onRestart() {
         super.onRestart()
--- app/src/main/java/com/example/sqlite/adapterproducto.kt
+++ app/src/main/java/com/example/sqlite/adapterproducto.kt
@@ -7,17 +7,23 @@ import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.android.gms.tasks.OnSuccessListener
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.itemrecycleproductos.view.*
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
 
-class adapterproducto(val listaproducto:ArrayList<producto>):RecyclerView.Adapter<adapterproducto.Viewholder>()
+class adapterproducto(val listaproducto:MutableList<DocumentSnapshot>):RecyclerView.Adapter<adapterproducto.Viewholder>()
 {
   //  private var db: room? = null
 
-    val database = FirebaseDatabase.getInstance().reference.child("Productos")
+
+    val db = FirebaseFirestore.getInstance()
+
 
     class Viewholder(itemview: View):RecyclerView.ViewHolder(itemview)
     {
@@ -32,26 +38,36 @@ class adapterproducto(val listaproducto:ArrayList<producto>):RecyclerView.Adapte
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): adapterproducto.Viewholder
     {
         val v=LayoutInflater.from(p0.context).inflate(R.layout.itemrecycleproductos,p0,false)
+       // Toast.makeText(v.context,"Dentro del adapter=="+listaproducto.size.toString(),Toast.LENGTH_SHORT).show()
+
         return Viewholder(v)
     }
     override fun getItemCount(): Int {
        return listaproducto.size
     }
     override fun onBindViewHolder(p0: adapterproducto.Viewholder, p1: Int) {
-        p0.tvnombre.setText(listaproducto.get(p1).nombre)
-        p0.tvprecio.setText(listaproducto.get(p1).precio.toString())
-        p0.tvcantidad.setText(listaproducto.get(p1).cantidad.toString())
+
+        p0.tvnombre.setText(listaproducto[p1].get("nombre").toString())
+        p0.tvprecio.setText(listaproducto[p1].get("precio").toString())
+        p0.tvcantidad.setText(listaproducto[p1].get("cantidad").toString())
 
 
         p0.delete.setOnClickListener {
-            database.child(listaproducto.get(p1).nombre.toString()).removeValue()
-          // notifyDataSetChanged()
+
+            db.collection("productos").document(listaproducto[p1].id).delete()
+                .addOnSuccessListener(OnSuccessListener {
+                       Toast.makeText(p0.itemView.context,"Producto Eliminado Correctamente",Toast.LENGTH_SHORT).show()
+                    })
+                .addOnFailureListener(OnFailureListener {
+                    Toast.makeText(p0.itemView.context,"Error al Eliminar",Toast.LENGTH_SHORT).show()
+                })
+
+
+
+        }
 
         }
 
 
-        p0.itemView.setOnClickListener {
-            Toast.makeText(p0.itemView.context,"Evento Click",Toast.LENGTH_SHORT).show()
-        }
+
      }
-}
